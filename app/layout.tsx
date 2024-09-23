f88cb272da8abd3fefffa0f4574ebd072d8070f8
antoniosarro <tech@antoniosarro.dev>
2024-09-23 20:13:42 +0200

    feat: added og meta tags

--- app/layout.tsx
+++ app/layout.tsx
@@ -20,6 +20,13 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="it">
+      <head>
+        <meta property="og:image" content="/og-image-cfp-1600x916.png" />
+        <meta property="og:image:width" content="1600" />
+        <meta property="og:image:height" content="916" />
+
+        <meta name="twitter:image" content="/og-image-cfp-1600x725.png" />
+      </head>
       <body
         className={`${manrope.className} container mx-auto px-4 py-4 antialiased`}
       >
@@ -27,9 +34,9 @@ export default function RootLayout({
           <Link href={"/"}>
             <img className="w-60 lg:w-80" src="/Lilis.svg" alt="LILIS Logo" />
           </Link>
-          <p className="hidden lg:block lg:text-xl lg:font-extrabold lg:text-accent lg:text-right">
-            Indirizzo /<br/>
-            Benevento (BN) /<br/>
+          <p className="hidden lg:block lg:text-right lg:text-xl lg:font-extrabold lg:text-accent">
+            Indirizzo /<br />
+            Benevento (BN) /<br />
             26 Ottobre /
           </p>
         </header>
