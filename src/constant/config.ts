0f14380e5ce77a38e6c21a653a412686331a3525
siddolo <me@pasqualefiorillo.it>
2026-09-09 11:36:28 +0200

    2025 to 2026

--- src/constant/config.ts
+++ src/constant/config.ts
@@ -1,11 +1,12 @@
 export const siteConfig = {
-  title: "Linux Day 2025 - LILIS",
+  title: "Linux Day 2026 - LILIS",
   description: "Evento annuale per promuovere Linux e il software libero",
   url: "https://cfp.lilis.it",
 };
 
 export const eventData = {
-  address: "Cubo Unisannio",
-  city: "Via dei Mulini, Benevento (BN)",
-  date: "25 Ottobre",
+  year: "2026",
+  address: "Sede dell'evento",
+  city: "in via di definizione",
+  date: "24 Ottobre 2026",
 };
